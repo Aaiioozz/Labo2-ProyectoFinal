6e5749056ba705b1646d6a0d74619db5deb1107c
Emanuel Rafael González <172980184+Aaiioozz@users.noreply.github.com>
2024-07-01 05:23:24 -0300

    1.6.2

--- Enemigo.cpp
+++ Enemigo.cpp
@@ -1,26 +1,50 @@
 #include "Enemigo.h"
 #include <ctime>
+#include <iostream>
+
+using namespace std;
 
  Enemigo::Enemigo(){
     texture.loadFromFile("enemigo_galaga.png");
     sprite.setTexture(texture);
     sprite.setPosition(450-sprite.getGlobalBounds().width/2, -sprite.getGlobalBounds().height);
  }
+
+ bool directionChange = false;
  void Enemigo::update(){
     if(sprite.getPosition().y<50){
         sprite.move(0, velocity);
     } else{
+        // Direccion del Enemigo Comun
         if(sprite.getPosition().x<=0||sprite.getPosition().x+sprite.getGlobalBounds().width>=900){
             velocity=-velocity;
+
+            if(!directionChange){
+                if(sprite.getPosition().y<=300){
+                sprite.move(0,50);
+                }
+                if(sprite.getPosition().y==300){
+                    directionChange = true;
+                    cout<<"cambio"<<endl;
+                }
+            }
+            else{
+                    sprite.move(0,-50);
+                    if(sprite.getPosition().y == 100){
+                    directionChange = false;
+                    }
+                }
+            }
+
         }
+
+        // Probabilidad de Disparo
         sprite.move(velocity, 0);
         if(std::rand()%100<2){
             Shoot();
         }
-    }
     disp.updateEnemigo();
- }
-
+}
 void Enemigo::Shoot(){
     if(disp.IsActive()==false){
         disp.setPosition(sprite.getPosition().x+sprite.getGlobalBounds().width/2, sprite.getPosition().y+sprite.getGlobalBounds().height);
